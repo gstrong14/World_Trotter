a1a29c443cae5a2ed47fe3451f2b405cef913982
Graham Strong <grahamstrong@Grahams-MacBook-Pro.local>
2017-02-08 13:50:09 -0500

    Through chapter 6

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -19,7 +19,49 @@ class MapViewController: UIViewController{
         
         // set it as *the* view of this view controller
         view = mapView
+        
+        let segmentedControl
+            = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor
+            = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.addTarget(self,
+                                   action: #selector(MapViewController.mapTypeCHanged(_:)),
+                                   for: .valueChanged)
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint
+            = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8)
+       
+        let margins = view.layoutMarginsGuide
+        
+        let leadingConstraint
+            = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint
+            = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
     }
+    
+    func mapTypeCHanged(_ segControl: UISegmentedControl){
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
